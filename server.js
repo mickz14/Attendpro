c45d95c33b0b7c81d12e97ff2d7380a23546863b
Sanya_jain <116953349+Jfury0127@users.noreply.github.com>
2024-11-03 22:52:17 +0530

    v
    b

--- server.js
+++ server.js
@@ -9,7 +9,8 @@ app.set("view engine","ejs");
 app.set("views",path.join(__dirname,"/views"));
 app.engine("ejs",ejsmate);
 
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({extended:true})); //  is a middleware provided by Express that will process the request encoded form and will put all information into the request body object you receive in your handler function.
+
 app.use(express.static(path.join(__dirname,"public")));
 
 app.get("/",(req,res)=>{
@@ -24,3 +25,34 @@ app.get("/teacher_login",(req,res)=>{
 app.listen(8080,()=>{
     console.log("server is listening on http://localhost:8080/")
 })
+
+
+// random code - ignore
+
+
+// const mysql = require("mysql"); 
+  
+// let db_con  = mysql.createConnection({ 
+//     host: "localhost", 
+//     user: "root", 
+//     password: '', 
+//     database: 'gfg_db'
+// }); 
+  
+// db_con.connect((err) => { 
+//     if (err) { 
+//       console.log("Database Connection Failed !!!", err); 
+//       return; 
+//     } 
+  
+//     console.log("We are connected to gfg_db database"); 
+  
+//     This query will be used to select columns 
+//     let query = 'SELECT * FROM users'; 
+  
+//     db_con.query(query, (err, rows) => { 
+//         if(err) throw err; 
+  
+//         console.log(rows); 
+//     }); 
+// });
