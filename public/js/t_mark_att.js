2d82b600879199a09ab6bbbd617d4a391b8c652a
Dhriti Luthra <92994956+dhritiluthra@users.noreply.github.com>
2024-11-08 11:44:14 +0530

    color change hehe

--- public/js/t_mark_att.js
+++ public/js/t_mark_att.js
@@ -1,4 +1,7 @@
 const attendanceDate = document.querySelector("#att-date");
+const takeAttendanceBtn = document.querySelector(".take-att-btn");
+const lectureSelect = document.getElementById("select-lect");
+const errorMessage = document.querySelector(".error-msg");
 
 // Function to format the date as YYYY-MM-DD
 function getFormattedDate(date) {
@@ -13,3 +16,16 @@ const today = new Date();
 const todayFormatted = getFormattedDate(today);
 
 attendanceDate.textContent = `${todayFormatted}`
+
+takeAttendanceBtn.addEventListener("click", () => {
+    if (lectureSelect.value === "") {
+        errorMessage.classList.remove("hidden"); // Show error message
+    } else {
+        errorMessage.classList.add("hidden"); // Hide error message if valid
+        // Continue with attendance-taking actions here
+    }
+});
+
+lectureSelect.addEventListener('focus',()=>{
+    errorMessage.classList.add("hidden");
+})
