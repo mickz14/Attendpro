d8399f20f663d84660c0d0787861f9ca0327eb43
Dhriti Luthra <92994956+dhritiluthra@users.noreply.github.com>
2024-11-22 01:47:49 +0530

    hi

--- database.js
+++ database.js
@@ -226,9 +226,10 @@ export async function getStudentInfofromENR(studentENR) {
 
 export async function fetchDetailedAttendance(enr,subId) {
    console.log(subId,enr);
+   console.log(typeof(subId));
    const [result] = await pool.query(
       `SELECT DATE_FORMAT(attendance_date, '%Y-%m-%d') AS attendance_date, status
-      FROM attendance where SUB_ID= '${subId}' AND ENR_NUMBER= ${enr};`)
+      FROM attendance where SUB_ID= ${subId} AND ENR_NUMBER= ${enr};`)
 
 
       console.log(result);
